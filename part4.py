78948886e30ea4b066f14666a9b59f2d14e04051
williamc15 <148293032+williamc15@users.noreply.github.com>
2024-07-23 17:27:18 +0300

    Update part4.py
    Another change

--- part4.py
+++ part4.py
@@ -1,7 +1,7 @@
 def stable_stock_matching(buyers_preferences, stocks_preferences):
     # Initialize all buyers and stocks as free
     free_buyers = list(buyers_preferences.keys())
-    matched_stocks = {}
+    matched_buyers = {}
     
     # Create a reverse preference list for stocks
     stocks_rank = {stock: {buyer: rank for rank, buyer in enumerate(buyers)}
@@ -10,16 +10,17 @@ def stable_stock_matching(buyers_preferences, stocks_preferences):
     while free_buyers:
         buyer = free_buyers.pop(0)
         for stock in buyers_preferences[buyer]:
-            if stock not in matched_stocks:
-                matched_stocks[stock] = buyer
+            if stock not in matched_buyers.values():
+                matched_buyers[buyer] = stock
                 break
             else:
-                current_buyer = matched_stocks[stock]
+                current_buyer = [b for b, s in matched_buyers.items() if s == stock][0]
                 if stocks_rank[stock][buyer] < stocks_rank[stock][current_buyer]:
                     free_buyers.append(current_buyer)
-                    matched_stocks[stock] = buyer
+                    matched_buyers[buyer] = stock
+                    del matched_buyers[current_buyer]
                     break
         else:
             free_buyers.append(buyer)
     
-    return {v: k for k, v in matched_stocks.items()}
+    return matched_buyers
