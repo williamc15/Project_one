4e3b18055c3f4d379253685535b07192351b61a5
williamc15 <148293032+williamc15@users.noreply.github.com>
2024-07-23 18:24:43 +0300

    Update part4.py
    Part4 again

--- part4.py
+++ part4.py
@@ -2,13 +2,13 @@ def stable_stock_matching(buyers_preferences, stocks_preferences):
     free_buyers = list(buyers_preferences.keys())
     matched_buyers = {}
     buyer_current_proposals = {buyer: 0 for buyer in free_buyers}
-    
+
     while free_buyers:
         buyer = free_buyers.pop(0)
         buyer_prefs = buyers_preferences[buyer]
         stock = buyer_prefs[buyer_current_proposals[buyer]]
         buyer_current_proposals[buyer] += 1
-        
+
         if stock not in matched_buyers.values():
             matched_buyers[buyer] = stock
         else:
@@ -20,20 +20,21 @@ def stable_stock_matching(buyers_preferences, stocks_preferences):
                 free_buyers.append(current_buyer)
             else:
                 free_buyers.append(buyer)
-    
-    return matched_buyers  # This is now correct, no need to reverse
+
+    return matched_buyers
 
 # Example usage
-if __name__ == "__main__":
-    buyers_preferences = {
-        'Buyer1': ['StockA', 'StockB', 'StockC'],
-        'Buyer2': ['StockB', 'StockA', 'StockC'],
-        'Buyer3': ['StockA', 'StockB', 'StockC']
-    }
-    stocks_preferences = {
-        'StockA': ['Buyer1', 'Buyer2', 'Buyer3'],
-        'StockB': ['Buyer2', 'Buyer1', 'Buyer3'],
-        'StockC': ['Buyer1', 'Buyer2', 'Buyer3']
-    }
-    result = stable_stock_matching(buyers_preferences, stocks_preferences)
-    print(result)
+buyers_preferences = {
+    'Buyer1': ['StockA', 'StockB', 'StockC'],
+    'Buyer2': ['StockB', 'StockA', 'StockC'],
+    'Buyer3': ['StockA', 'StockB', 'StockC']
+}
+
+stocks_preferences = {
+    'StockA': ['Buyer1', 'Buyer2', 'Buyer3'],
+    'StockB': ['Buyer2', 'Buyer1', 'Buyer3'],
+    'StockC': ['Buyer1', 'Buyer2', 'Buyer3']
+}
+
+result = stable_stock_matching(buyers_preferences, stocks_preferences)
+print(result)
